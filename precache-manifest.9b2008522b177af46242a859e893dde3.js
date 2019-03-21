6b5f80f0cca2c32a3780c496a33279628f5174e0
Manuel Gomez <mgomez@gemini.edu>
2019-03-21 14:18:47 -0300

    Updates

---
+++ precache-manifest.9b2008522b177af46242a859e893dde3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d71379f2778062e586e",
+    "url": "/star-pairs/static/js/runtime~main.b5c12489.js"
+  },
+  {
+    "revision": "0159eef836d6cbdd53f0",
+    "url": "/star-pairs/static/js/main.562aa9c7.chunk.js"
+  },
+  {
+    "revision": "3d052684392469d995e4",
+    "url": "/star-pairs/static/js/2.eaffa79b.chunk.js"
+  },
+  {
+    "revision": "0159eef836d6cbdd53f0",
+    "url": "/star-pairs/static/css/main.c9204275.chunk.css"
+  },
+  {
+    "revision": "35c5cfa467c0a321d28ad7b7ef3b184a",
+    "url": "/star-pairs/index.html"
+  }
+];
